f278fde59ff1c16a508f070c765fade2883715e4
hannahcabug <71950557+hannahcabug@users.noreply.github.com>
2022-01-11 16:20:36 -0800

    RPS game page

---
+++ web/pages/rpsgame.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import React, {useState} from 'react';
+import CircleText from '../comps/CircleText';
+import Button from '../comps/Button';
+
+const Holder = styled.div `
+.container1 {
+    display:flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items:center;
+};
+.container2 {
+    display:flex;
+    flex-direction: row;
+    justify-content:center;
+    align-items:center;
+};
+`;
+
+export default function RPS() {
+
+    return <Holder>
+        <div className ="container1">
+            <h1>PLAYER 1</h1>
+            <h3>Pick One</h3>
+            <div className ="container2">
+            <CircleText></CircleText>
+            <CircleText text='Paper' back='#85A9A1'></CircleText>
+            <CircleText text='Scissors' back='#D3E1C5'></CircleText> 
+            </div>  
+              
+            <Button text='Done'></Button>
+        </div>
+
+    </Holder>
+}
