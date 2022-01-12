2add3a8f47414f91e70076859872b57ac25e485e
JacobCordes <jcordes@my.bcit.ca>
2022-01-11 16:26:22 -0800

    Finished comp

--- web/comps/Button/BigGameLogo/index.js
+++ web/comps/Button/BigGameLogo/index.js
@@ -3,21 +3,32 @@ import styled from 'styled-components';
 
 const Circle = styled.div`
 display:flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
 width:274px;
 height:274px;
 background-color:${props=>props.bgcolor};
 border-radius:150px;
 `
 
+const Image = styled.img`
+display: flex;
+width:${props=>props.width};
+height:${props=>props.height};
+align-items: center;
+justify-content: center;
+`;
+
 const BigGameLogo = ({
 bgcolor="#FAD",
-svg="",
+img="https://www.paradigm.com/2676-home_default/rock-monitor-60-sm.jpg",
 
 }) => {
     return (
     <div>
-        <Circle bgcolor="#58778A">
-   
+        <Circle bgcolor={bgcolor}>
+        <Image width={width} height={height} src={src}></Image>
             </Circle>
     </div>
 
